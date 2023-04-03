6f50d26a9aff7bafee2eb4a99acf192c50d48052
Sushrut Patil <patilsushrut75@gmail.com>
2023-04-04 01:43:50 +0530

    Code Cleanup

--- ArtSubmissionPage.java
+++ ArtSubmissionPage.java
@@ -9,53 +9,52 @@ import java.sql.*;
 
 public class ArtSubmissionPage extends JFrame implements ActionListener {
     int counter;
-    private final JButton  submitButton,cancelbutton,ImagePreviewbutton;
-    private  JLabel title,pagetitle,ImagePreviewLabel,ImageChooseLabel;
-    private JFileChooser fileChooser;
-    private JLabel ArtName,Artist,Artype,Height,Width,Price,Descriptions;
-    private JTextField ArtNameField,ArtistField,HeightField,WidthField,PriceField;
-    private JTextArea DescriptionsField;
-    private JComboBox<String> ArttypeField;
-    private JPanel Submissionpanel,ImagePreviewPanel;
+    private final JButton ImagePreviewbutton;
+    private final JFileChooser fileChooser;
+    private final JTextField ArtNameField,ArtistField,HeightField,WidthField,PriceField;
+
+    private final JTextArea DescriptionsField;
+    private final JComboBox<String> ArttypeField;
+    private final JPanel ImagePreviewPanel;
     private File SelectedFile;
-    private String username;
+    private final String username;
     public ArtSubmissionPage(String username,int counter) {
         super("Art Submission Page");
         this.username = username;
         this.counter = counter;
-        username = this.username;
+
         //Main Title
-        title = new JLabel("Digital Art Gallery ",SwingConstants.CENTER);
+        JLabel title = new JLabel("Digital Art Gallery ", SwingConstants.CENTER);
         title.setFont(new Font("Verdana", Font.BOLD, 40));
         title.setForeground(Color.RED);
         title.setBounds(50,10,500,100);
         add(title);
 
         //Page Title
-        pagetitle = new JLabel("Art Submission Page",SwingConstants.CENTER);
+        JLabel pagetitle = new JLabel("Art Submission Page", SwingConstants.CENTER);
         pagetitle.setFont(new Font("Verdana", Font.PLAIN, 20));
         pagetitle.setForeground(Color.BLACK);
         pagetitle.setBounds(185,100,250,50);
         add(pagetitle);
 
-        ImageChooseLabel = new JLabel("Image Chooser",SwingConstants.CENTER);
-        ImageChooseLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
-        ImageChooseLabel.setForeground(Color.BLACK);
-        ImageChooseLabel.setBounds(950,10,200,50);
-        add(ImageChooseLabel);
+        JLabel imageChooseLabel = new JLabel("Image Chooser", SwingConstants.CENTER);
+        imageChooseLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
+        imageChooseLabel.setForeground(Color.BLACK);
+        imageChooseLabel.setBounds(950,10,200,50);
+        add(imageChooseLabel);
 
-        ImagePreviewLabel = new JLabel("Image Preview",SwingConstants.CENTER);
-        ImagePreviewLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
-        ImagePreviewLabel.setForeground(Color.BLACK);
-        ImagePreviewLabel.setBounds(950,330,200,50);
-        add(ImagePreviewLabel);
+        JLabel imagePreviewLabel = new JLabel("Image Preview", SwingConstants.CENTER);
+        imagePreviewLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
+        imagePreviewLabel.setForeground(Color.BLACK);
+        imagePreviewLabel.setBounds(950,330,200,50);
+        add(imagePreviewLabel);
 
         ImagePreviewPanel = new JPanel();
         ImagePreviewPanel.setBounds(650,380,800,450);
         add(ImagePreviewPanel);
 
 
-        submitButton = new JButton("Submit");
+        JButton submitButton = new JButton("Submit");
         submitButton.addActionListener(this);
         submitButton.setBounds(100,700,100,30);
         add(submitButton);
@@ -65,7 +64,7 @@ public class ArtSubmissionPage extends JFrame implements ActionListener {
         ImagePreviewbutton.setBounds(240,700,120,30);
         add(ImagePreviewbutton);
 
-        cancelbutton = new JButton("Cancel");
+        JButton cancelbutton = new JButton("Cancel");
         cancelbutton.addActionListener(this);
         cancelbutton.setBounds(400,700,100,30);
         add(cancelbutton);
@@ -77,49 +76,49 @@ public class ArtSubmissionPage extends JFrame implements ActionListener {
         add(fileChooser);
 
 
-        Submissionpanel = new JPanel(new GridLayout(7,2,5,5));
-        Submissionpanel.setBounds(100,200,400,400);
-        Submissionpanel.setBackground(Color.YELLOW);
+        JPanel submissionpanel = new JPanel(new GridLayout(7, 2, 5, 5));
+        submissionpanel.setBounds(100,200,400,400);
+        submissionpanel.setBackground(Color.YELLOW);
 
-        ArtName = new JLabel(" Art Name :");
-        Submissionpanel.add(ArtName);
+        JLabel artName = new JLabel(" Art Name :");
+        submissionpanel.add(artName);
         ArtNameField = new JTextField();
-        Submissionpanel.add(ArtNameField);
+        submissionpanel.add(ArtNameField);
 
-        Artist = new JLabel(" Artist Name :");
-        Submissionpanel.add(Artist);
+        JLabel artist = new JLabel(" Artist Name :");
+        submissionpanel.add(artist);
         ArtistField = new JTextField();
-        Submissionpanel.add(ArtistField);
+        submissionpanel.add(ArtistField);
 
-        Artype = new JLabel(" Art-Type :");
-        Submissionpanel.add(Artype);
+        JLabel artype = new JLabel(" Art-Type :");
+        submissionpanel.add(artype);
         String[] listarttype = { "Painting", "AI-Art", "Photography", "Sketch", "Pixel Art","Photo Painting", "Others" };
         ArttypeField = new JComboBox<>(listarttype);
 
-        Submissionpanel.add(ArttypeField);
+        submissionpanel.add(ArttypeField);
 
-        Height = new JLabel(" Height : ");
-        Submissionpanel.add(Height);
+        JLabel height = new JLabel(" Height : ");
+        submissionpanel.add(height);
         HeightField = new JTextField();
-        Submissionpanel.add(HeightField);
+        submissionpanel.add(HeightField);
 
-        Width = new JLabel(" Width : ");
-        Submissionpanel.add(Width);
+        JLabel width = new JLabel(" Width : ");
+        submissionpanel.add(width);
         WidthField = new JTextField();
-        Submissionpanel.add(WidthField);
+        submissionpanel.add(WidthField);
 
 
-        Price = new JLabel(" Price : ");
-        Submissionpanel.add(Price);
+        JLabel price = new JLabel(" Price : ");
+        submissionpanel.add(price);
         PriceField = new JTextField();
-        Submissionpanel.add(PriceField);
+        submissionpanel.add(PriceField);
 
 
-        Descriptions = new JLabel(" Description : ");
-        Submissionpanel.add(Descriptions);
+        JLabel descriptions = new JLabel(" Description : ");
+        submissionpanel.add(descriptions);
         DescriptionsField = new JTextArea();
-        Submissionpanel.add(DescriptionsField);
-        add(Submissionpanel);
+        submissionpanel.add(DescriptionsField);
+        add(submissionpanel);
 
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -146,10 +145,9 @@ public class ArtSubmissionPage extends JFrame implements ActionListener {
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/DigitalArtGallery", "root", "root");
     }
-    public boolean ArtSubmission(String ArtName,String ArtistName,String ArtType,String Height,String Width,String Price,String Description) {
-        boolean submitflag = false;
-        Connection connection = null;
-        PreparedStatement statement1,statement2 = null;
+    public boolean ArtSubmission(String ArtName,String ArtistName,String ArtType,Integer Height,Integer Width,Integer Price,String Description) {
+        Connection connection;
+        PreparedStatement statement1,statement2;
         int num;
         try {
             connection = getConnection();
@@ -172,18 +170,18 @@ public class ArtSubmissionPage extends JFrame implements ActionListener {
             statement2.setString(2,ArtName);
             statement2.setString(3,ArtistName);
             statement2.setString(4,ArtType);
-            statement2.setString(5,Height);
-            statement2.setString(6,Width);
-            statement2.setString(7,Price);
+            statement2.setInt(5,Height);
+            statement2.setInt(6,Width);
+            statement2.setInt(7,Price);
             statement2.setString(8,Description);
 
             num = statement1.executeUpdate();
             if (num==0){
-                return submitflag;
+                return true;
             }
             num = statement2.executeUpdate();
             if (num!=0) {
-                submitflag = true;
+                return true;
             }
             statement1.close();
             statement2.close();
@@ -192,7 +190,7 @@ public class ArtSubmissionPage extends JFrame implements ActionListener {
         }catch (SQLException | IOException e) {
             e.printStackTrace();
         }
-        return submitflag;
+        return false;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -203,7 +201,7 @@ public class ArtSubmissionPage extends JFrame implements ActionListener {
                 this.SelectedFile = fileChooser.getSelectedFile();
                 System.out.println(SelectedFile);
                 ArtPreview();
-            } else if (Returnval == fileChooser.CANCEL_OPTION) {
+            } else if (Returnval == JFileChooser.CANCEL_OPTION) {
                     //TODO : Working of Cancel button in File Chooser
                     //TODO : Refreshing Image Viewer Panel
             }
@@ -215,16 +213,17 @@ public class ArtSubmissionPage extends JFrame implements ActionListener {
             String ArtName = ArtNameField.getText();
             String ArtistName = ArtistField.getText();
             String ArtType = (String) ArttypeField.getSelectedItem();
-            String Height = HeightField.getText();
-            String Width = WidthField.getText();
-            String Price = PriceField.getText();
+            Integer Height = Integer.valueOf(HeightField.getText());
+            Integer Width = Integer.valueOf(WidthField.getText());
+            Integer Price = Integer.valueOf(PriceField.getText());
             String Description = DescriptionsField.getText();
 
-            if (ArtName.isEmpty() || ArtistName.isEmpty() || Height.isEmpty() || Width.isEmpty() || Price.isEmpty()) {
+            if (ArtName.isEmpty() || ArtistName.isEmpty() || Height.equals(0) || Width.equals(0) || Price.equals(0)) {
                 JOptionPane.showMessageDialog(this, "Please enter all fields");
             } else if (ArtSubmission(ArtName,ArtistName,ArtType,Height,Width,Price,Description)) {
                 JOptionPane.showMessageDialog(this, "Art Submission successful");
-                new HomePage(username,1);
+                new HomePage(username,counter);
+                //TODO Return to Page of Newly Inserted Art
                 setVisible(false);
             } else {
                 JOptionPane.showMessageDialog(this, "Please Enter Correct Details");
