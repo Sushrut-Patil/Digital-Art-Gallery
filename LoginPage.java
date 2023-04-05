d959b1357733b67b5cb76d0bba60d46064e86656
Sushrut Patil <patilsushrut75@gmail.com>
2023-04-05 13:40:44 +0530

    Login Page

--- LoginPage.java
+++ LoginPage.java
@@ -10,29 +10,27 @@ import java.sql.*;
 public class LoginPage extends JFrame implements ActionListener {
 
     JPanel panel;
-    private final JLabel title,pagetitle,usernameLabel,passwordLabel;
 
     private final JTextField usernameField;
-    private  JPasswordField passwordField;
+    private final JPasswordField passwordField;
 
-    private JButton loginbutton,toregisterpage;
-    int counter = 100;
     public LoginPage() {
 
         super("Login Page");
+//        To Set Background Image to JFrame
         try {
             setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src/Images/LoginPage.JPEG")))));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        title = new JLabel("Digital Art Gallery ",SwingConstants.CENTER);
+        JLabel title = new JLabel("Digital Art Gallery ", SwingConstants.CENTER);
         title.setFont(new Font("Verdana", Font.BOLD, 40));
         title.setForeground(Color.RED);
         title.setBounds(500,100,500,100);
         add(title);
 
-        pagetitle = new JLabel("Login Page",SwingConstants.CENTER);
+        JLabel pagetitle = new JLabel("Login Page", SwingConstants.CENTER);
         pagetitle.setFont(new Font("Verdana", Font.PLAIN, 20));
         pagetitle.setForeground(Color.BLACK);
         pagetitle.setBounds(650,250,200,50);
@@ -43,45 +41,49 @@ public class LoginPage extends JFrame implements ActionListener {
         panel.setOpaque(false);
         add(panel);
 
-        usernameLabel = new JLabel("Username:",SwingConstants.CENTER);
+        JLabel usernameLabel = new JLabel("Username:", SwingConstants.CENTER);
         panel.add(usernameLabel);
         usernameField = new JTextField();
         panel.add(usernameField);
 
-        passwordLabel = new JLabel("Password :",SwingConstants.CENTER);
+        JLabel passwordLabel = new JLabel("Password :", SwingConstants.CENTER);
         panel.add(passwordLabel);
         passwordField = new JPasswordField();
         panel.add(passwordField);
 
-        loginbutton = new JButton("Login");
+        JButton loginbutton = new JButton("Login");
         loginbutton.setBackground(Color.BLUE);
         loginbutton.setForeground(Color.WHITE);
         loginbutton.setBounds(700,500,100,30);
         add(loginbutton);
         loginbutton.addActionListener(this);
 
-        toregisterpage = new JButton("Don't have Account?Create One!!");
+        JButton toregisterpage = new JButton("Don't have Account?Create One!!");
         toregisterpage.setForeground(Color.BLUE);
         toregisterpage.setBounds(620,550,250,30);
         add(toregisterpage);
         toregisterpage.addActionListener(this);
 
+//        Frame Settings
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setVisible(true);
 
     }
+//   Login Function
     private boolean LoginFunction(String username,String Password) {
         boolean loggedIn = false;
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
+        Connection connection;
+        PreparedStatement statement;
+        ResultSet resultSet;
 
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/DigitalArtGallery", "root", "root");
+//            Connects to Database
+            connection = Main.getConnection();
 
             String sql = "SELECT * FROM users WHERE username = ? AND password = MD5(?)";
+
             statement = connection.prepareStatement(sql);
             statement.setString(1, username);
             statement.setString(2, Password);
@@ -91,7 +93,7 @@ public class LoginPage extends JFrame implements ActionListener {
             if (resultSet.next()) {
                 loggedIn = true;
             }
-
+//            Close Connection, ResultSet and Statement
             resultSet.close();
             statement.close();
             connection.close();
@@ -114,7 +116,10 @@ public class LoginPage extends JFrame implements ActionListener {
             } else if (LoginFunction(username,password)) {
 
                 JOptionPane.showMessageDialog(this, "Login Successful");
-                new HomePage(username,counter);
+//                On Successful Login new HomePage Opens
+                Main.setUsername(username);
+                Main.RandomCounter();
+                new HomePage();
                 setVisible(false);
 
             } else{
