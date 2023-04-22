b88ce9e7f9ff0146ee93094cbe5d708e5f3e9820
Sushrut Patil <patilsushrut75@gmail.com>
2023-04-23 00:48:17 +0530

    Show Balance Feature

--- MyAccount.java
+++ MyAccount.java
@@ -12,87 +12,94 @@ import java.util.ArrayList;
 
 public class MyAccount extends BaseFrame implements ActionListener {
 
-    JButton logout,Updateart,returntohome;
-    JLabel ValUser_Id,ValUsername,ValName,ValEmail,ValArtOwned;
+    final JButton logout;
+    final JButton returntohome;
+    final JLabel ValUser_Id;
+    final JLabel ValUsername;
+    final JLabel ValName;
+    final JLabel ValEmail;
+    final JLabel ValArtOwned;
+    private final CustomLabel ValBalance;
     int ArtsCount;
+
     public MyAccount() {
         super("My Account Page");
 
         //Main Title of Application
         JLabel title = new MainTitle();
-        title.setBounds(500,20,500,100);
+        title.setBounds(500, 20, 500, 100);
         add(title);
 
         //Page Title
-        JLabel pagetitle = new PageTitles("MY ACCOUNT PAGE");
-        pagetitle.setBounds(650,120,200,50);
+        JLabel pagetitle = new PageTitles(" MY ACCOUNT PAGE");
+        pagetitle.setBounds(630, 120, 250, 50);
         add(pagetitle);
 
-        JLabel ArtTabletitle = new PageTitles("My Art Collection");
-        ArtTabletitle.setBounds(900,200,200,50);
+        JLabel ArtTabletitle = new PageTitles(" My Art Collection");
+        ArtTabletitle.setBounds(900, 200, 200, 50);
         add(ArtTabletitle);
 
         //Function To Display ArtDetails Owned By User
         DetailsTable();
 
-        JLabel UserDetails = new PageTitles("User Details");
-        UserDetails.setBounds(125,200,250,50);
+        JLabel UserDetails = new PageTitles(" User Details");
+        UserDetails.setBounds(125, 200, 250, 50);
         add(UserDetails);
 
         //Panel to Display Users Details
-        JPanel UserPanel = new JPanel(new GridLayout(5,2,4,4));
-        UserPanel.setBounds(50,250,400,300);
+        JPanel UserPanel = new JPanel(new GridLayout(6, 2, 5, 5));
+        UserPanel.setBounds(50, 250, 400, 300);
         UserPanel.setBackground(Color.PINK);
         add(UserPanel);
 
-        JLabel User_Id = new CustomLabel("User_ID");
+        JLabel User_Id = new CustomLabel(" User_ID");
         UserPanel.add(User_Id);
         ValUser_Id = new CustomLabel(" ");
         UserPanel.add(ValUser_Id);
 
-        JLabel User_Name = new CustomLabel("Username");
+        JLabel User_Name = new CustomLabel(" Username");
         UserPanel.add(User_Name);
         ValUsername = new CustomLabel(" ");
         UserPanel.add(ValUsername);
 
-        JLabel Name = new CustomLabel("Name");
+        JLabel Name = new CustomLabel(" Name");
         UserPanel.add(Name);
         ValName = new CustomLabel(" ");
         UserPanel.add(ValName);
 
-        JLabel Email = new CustomLabel("Email");
+        JLabel Email = new CustomLabel(" Email");
         UserPanel.add(Email);
-        ValEmail= new CustomLabel(" ");
+        ValEmail = new CustomLabel(" ");
         UserPanel.add(ValEmail);
 
-        JLabel ArtOwned= new CustomLabel("Arts Owned");
+        JLabel ArtOwned = new CustomLabel(" Arts Owned");
         UserPanel.add(ArtOwned);
         ValArtOwned = new CustomLabel("");
         UserPanel.add(ValArtOwned);
+
+        JLabel Balance = new CustomLabel(" Balance");
+        UserPanel.add(Balance);
+        ValBalance = new CustomLabel("");
+        UserPanel.add(ValBalance);
 
         //Method to Fetch User Details from Database
         FetchUserDetails();
 
         //LogOut Button
-        logout = new CustomButtons("LOG OUT",Color.RED);
+        logout = new CustomButtons("LOG OUT", Color.RED);
         logout.addActionListener(this);
-        logout.setBounds(200,570,100,30);
+        logout.setBounds(200, 600, 100, 30);
         add(logout);
 
-        //Update Art Details
-        Updateart = new CustomButtons("UPDATE ART DETAILS",Color.BLUE);
-        Updateart.addActionListener(this);
-        Updateart.setBounds(900,570,200,30);
-        add(Updateart);
-
         //Return to HomePage
-        returntohome = new CustomButtons("<<< HOME",Color.GREEN);
-        returntohome.setBounds(10,10,150,30);
+        returntohome = new CustomButtons("<<< HOME", Color.GREEN);
+        returntohome.setBounds(10, 10, 150, 30);
+        returntohome.setForeground(Color.BLACK);
         returntohome.addActionListener(this);
         add(returntohome);
     }
 
-    public void DetailsTable()  {
+    public void DetailsTable() {
 
         String[] columnNames = {"ART ID", "ART NAME", "ART TYPE", "HEIGHT", "WIDTH", "PRICE", "AVAILABILITY"};
         JTable Table = new JTable((Object[][]) FetchArtDetails(), columnNames);
@@ -103,9 +110,10 @@ public class MyAccount extends BaseFrame implements ActionListener {
         Table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         JScrollPane TableContainer = new JScrollPane(Table);
 
-        TableContainer.setBounds(500,250,1000,300);
+        TableContainer.setBounds(500, 250, 1000, 300);
         add(TableContainer);
     }
+
     public Object FetchArtDetails() {
         Connection connection;
         PreparedStatement statement;
@@ -118,7 +126,7 @@ public class MyAccount extends BaseFrame implements ActionListener {
             statement = connection.prepareStatement(sql);
             statement.setString(1, Main.getUsername());
             rs = statement.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 Object[] object = new Object[]{rs.getInt(1),
                         rs.getString(2),
                         rs.getString(3),
@@ -142,24 +150,26 @@ public class MyAccount extends BaseFrame implements ActionListener {
 
         return Data;
     }
-    private void FetchUserDetails(){
+
+    private void FetchUserDetails() {
 
         Connection connection;
         PreparedStatement statement;
         ResultSet rs;
         try {
             connection = Main.getConnection();
-            String sql = "SELECT USER_ID,NAME,EMAIL FROM USERS WHERE USERNAME = (?)";
+            String sql = "SELECT USER_ID,NAME,EMAIL,BALANCE FROM USERS WHERE USERNAME = (?)";
             statement = connection.prepareStatement(sql);
             statement.setString(1, Main.getUsername());
             rs = statement.executeQuery();
             rs.next();
 
-            ValUser_Id.setText(rs.getString(2));
+            ValUser_Id.setText(rs.getString(1));
             ValUsername.setText(Main.getUsername());
             ValName.setText(rs.getString(2));
             ValEmail.setText(rs.getString(3));
             ValArtOwned.setText(String.valueOf(ArtsCount));
+            ValBalance.setText(String.valueOf(rs.getString(4)));
 
             rs.close();
             statement.close();
@@ -179,8 +189,6 @@ public class MyAccount extends BaseFrame implements ActionListener {
                 new LoginPage();
                 dispose();
             }
-        } else if (e.getSource().equals(Updateart)) {
-            //TODO : Create Update Page
         } else if (e.getSource().equals(returntohome)) {
             new HomePage();
             dispose();
